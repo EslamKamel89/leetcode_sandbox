0cfb6e476301e1ba6fd52fff96cd074ccd5666ac
EslamKamel89 <elsamkamelforex@gmail.com>
2026-05-06 13:19:03 +0300

    refactor(prefix-sum): optimize continuous subarray sum solution with remainder caching and edge case handling

--- course/section_1_array_and_hasing/code/lesson_15_checkSubarraySum.py
+++ course/section_1_array_and_hasing/code/lesson_15_checkSubarraySum.py
@@ -4,23 +4,28 @@ from typing import List
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
         """
-         subarray[i:j] % k = 0
-        ( prefix[i] - prefix[j] ) % k = 0
-         prefix[i] % k - prefix[j] % k = 0
-         prefix[i] % k  = prefix[j] % k
+        prefix[i:j] % k = 0
+        prefix[i] % k - prefix[i] % k = 0
+        prefix[i] % k  = prefix[j] % k
         """
-        print("nums = ", nums)
-        seen: dict[int, int] = {}
-        sum = 0
+        if len(nums) < 2:
+            return False
+        if k == 1:
+            return True
+        seen = {0: -1}
+        prefix_sum = 0
+        # print('nums = ' , nums)
         for i, num in enumerate(nums):
-            print(f" ---------------- {i} ---------------- ")
-            sum += num
-            rem = sum % k
-            print("sum = ", sum)
-            print("rem = ", rem)
-            if rem in seen:
+            # print('---------------------------')
+            # print('i = ' , i )
+            # print('num = ' , num)
+            prefix_sum += num
+            rem = prefix_sum % k
+            # print('prefix_sum = ' , prefix_sum)
+            # print('rem = ' , rem )
+            if rem not in seen:
+                seen[rem] = i
+            elif i - seen[rem] > 1:
                 return True
-            seen[sum % k] = i
 
-        print("seen = ", seen)
         return False

da6d482393b32268841c157bb574fe1dd988f613
EslamKamel89 <elsamkamelforex@gmail.com>
2026-06-06 11:22:33 +0300

    docs(review): update solved problems tracker, add review randomizer, and remove Continuous Subarray Sum notes

---
+++ course/review/random_generator.py
@@ -0,0 +1,11 @@
+import random
+
+n = 6 
+max = 87 
+
+if n > max:
+    raise ValueError("n cannot be greater than max when numbers must be unique.")
+
+numbers = random.sample(range(1,max+1) , n)
+numbers.sort()
+print(numbers)

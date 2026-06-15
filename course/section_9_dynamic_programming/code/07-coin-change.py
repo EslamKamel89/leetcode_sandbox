36c8ac493cd111bc1ff7d6ad4a0d6baee0e02b2d
EslamKamel89 <elsamkamelforex@gmail.com>
2026-06-15 22:06:23 +0300

    feat(dp): add Coin Change lesson with bottom-up DP solution and visualization

---
+++ course/section_9_dynamic_programming/code/07-coin-change.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        coins.sort()
+        dp = [0] * (amount + 1)
+        for i in range(1, amount + 1):
+            minn = float("inf")
+            for coin in coins:
+                diff = i - coin
+                if diff < 0:
+                    break
+                minn = min(minn, dp[diff] + 1)
+            dp[i] = minn # type: ignore
+        return dp[amount] if dp[amount] < float("inf") else -1
